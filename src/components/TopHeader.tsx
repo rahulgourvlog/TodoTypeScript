b10f9e0ce5f47ff477f059ec250c7ec315301f6d
Rahul gour <96679977+rahulgourvlog@users.noreply.github.com>
2024-07-25 06:50:43 +0530

    Update TopHeader.tsx

--- src/components/TopHeader.tsx
+++ src/components/TopHeader.tsx
@@ -1,12 +1,19 @@
-// import React, { useState } from 'react'
-// import { Link } from 'react-router-dom'
-// import Button from './Button'
+
+import { Link, useLocation } from "react-router-dom"
+import Button from "./Button"
 
 const TopHeader = () => {
-   
+  const location = useLocation();
   return (
     <>
-   
+    <div className="flex flex-col sm:flex-row justify-between   mb-4">
+          <div className="text-4xl">Todo App</div>
+          {location.pathname !== "/addtodo" && (
+         <Link to="/addtodo" className="mt-4 sm:mt-0">
+         <Button >+ AddTodo</Button>
+         </Link>)
+}
+</div>
     </>
   )
 }
